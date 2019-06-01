91b6e879255d19e49b909e5dad4257b6f22b3458
zbynioo85 <zbynioo85@gmail.com>
2019-06-01 16:45:49 +0200

    na zajeciach

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,22 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="https://cdn.galleries.smcloud.net/t/galleries/gf-UNto-nrdS-94kd_krzysztof-krawczyk-664x442-nocrop.JPG">
+   
+    <HelloWorld msg="Krawczyk po 10 piwach, wchodzi lepiej"/>
+    <Test msg="krawczyk komponent"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Test
   }
 }
 </script>
 
-<style>
+<style type="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
